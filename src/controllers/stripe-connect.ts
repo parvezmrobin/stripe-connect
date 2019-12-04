a9fb7b81409be6d7844d61a8329b2844fda8e5df
Parvez M Robin <parvezmrobin@gmail.com>
2019-12-04 22:11:51 +0600

    stripe connect bugfix

--- src/controllers/stripe-connect.ts
+++ src/controllers/stripe-connect.ts
@@ -1,24 +1,25 @@
 import {Request, Response} from "express";
-import axios from "axios";
 import {StripePayload, User, UserDocument} from "../models/User";
+import Stripe from "stripe";
+import logger from "../util/logger";
 
-export const validateStripeCall = (req: Request, res: Response) => {
+const stripeClient = new Stripe("sk_test_J4Z45kIzrgJ1GFy5qR0ovQ7L00B8lfIVr4");
+export const validateStripeCall = async (req: Request, res: Response) => {
     /* eslint-disable @typescript-eslint/camelcase */
-    const url = "https://connect.stripe.com/oauth/token";
-    axios
-        .post(url, {
-            client_secret: "sk_test_J4Z45kIzrgJ1GFy5qR0ovQ7L00B8lfIVr4",
-            code: "ac_GIaJc9E6xfTZ4PU1XQT1XYS1gan87lSS",
-            grant_type: "authorization_code",
-        })
-        .then(async stripeResponse => {
-            const payload = stripeResponse.data as StripePayload;
-            if (payload.stripe_publishable_key !== "pk_test_asIQM8Tg70uCLDiL7dqoAXQA00XaUKZEqC") {
-                return res.json({success: false});
-            }
+    try {
+        const stripeResponse: StripePayload = await stripeClient.oauth
+            .token({
+                grant_type: "authorization_code",
+                code: "ac_GIaJc9E6xfTZ4PU1XQT1XYS1gan87lSS",
+            });
+        if (stripeResponse.stripe_publishable_key !== "pk_test_asIQM8Tg70uCLDiL7dqoAXQA00XaUKZEqC") {
+            return res.json({success: false});
+        }
 
-            const user = req.user as UserDocument;
-            await User.updateOne({_id: user._id}, {$set: {stripe: payload}});
-            return res.redirect("/account");
-        });
+        const user = req.user as UserDocument;
+        await User.updateOne({_id: user._id}, {$set: {stripe: stripeResponse}});
+    } catch (e) {
+        logger.error(e.statusCode, e.type, e.message);
+    }
+    return res.redirect("/account");
 };
