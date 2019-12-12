145340f9d73b74fd21e3f800140662ddbf635e82
Parvez M Robin <parvezmrobin@gmail.com>
2019-12-12 19:53:10 +0600

    stripe payment backend

--- src/controllers/stripe.ts
+++ src/controllers/stripe.ts
@@ -2,6 +2,7 @@ import {Request, Response} from "express";
 import {StripePayload, User, UserDocument} from "../models/User";
 import Stripe from "stripe";
 import logger from "../util/logger";
+import {BookDocument, BookStore} from "../models/BookStore";
 
 const stripeClient = new Stripe("sk_test_J4Z45kIzrgJ1GFy5qR0ovQ7L00B8lfIVr4");
 export const validateStripeCall = async (req: Request, res: Response) => {
@@ -28,7 +29,35 @@ export const getPaymentForm = (req: Request, res: Response) => {
     res.render("payment");
 };
 
-export const charge = (req: Request, res: Response) => {
-  logger.debug(req.body);
-  res.json(req.body);
+export const charge = async (req: Request, res: Response) => {
+    logger.debug(req.body);
+    let book: BookDocument;
+    try {
+        const bookId = req.body.id;
+        const bookStore = await BookStore.findOne({"books._id": bookId}).exec();
+        book = await bookStore.books.find(b => b._id.toString() === bookId);
+        const user = await User.findOne({_id: bookStore.user}).exec();
+
+        // Token is created using Stripe Checkout or Elements!
+        // Get the payment token ID submitted by the form:
+        const token = req.body.stripeToken; // Using Express
+        const charge = await stripeClient.charges.create({
+            amount: Math.ceil(book.price * 100), // price in cents
+            currency: "aud",
+            description: `Payment for book '${book.name}' from ${bookStore.name}`,
+            source: token,
+            application_fee_amount: Math.ceil(book.price * 10),
+        }, {
+            stripe_account: user.stripe.stripe_user_id,
+        });
+        const sale = {...charge, book: book._id};
+        await BookStore.updateOne({_id: bookStore._id}, {$push: {sales: sale}});
+        req.flash("success", [{msg: "Successfully brought book " + book.name}]);
+        logger.debug(charge);
+        res.redirect("/surf");
+    } catch (e) {
+        logger.error(e);
+        req.flash("errors", [{msg: "Failed to purchase book " + book.name}]);
+        res.redirect("/surf");
+    }
 };
--- src/models/BookStore.ts
+++ src/models/BookStore.ts
@@ -1,23 +1,28 @@
 import mongoose, {Query} from "mongoose";
+import {charges} from "stripe";
 
 const ObjectId = mongoose.Schema.Types.ObjectId;
 type ObjectId = mongoose.Types.ObjectId;
 type addBookFunction = (name: string, price: number) => Query<BookStoreDocument>;
 
+export type BookDocument = mongoose.Document & {
+    name: string;
+    price: number;
+};
+type ChargeDocument = mongoose.Document & charges.ICharge;
 export type BookStoreDocument = mongoose.Document & {
     user: ObjectId;
     name: string;
-    books: Array<{
-       name: string;
-       price: number;
-    }>;
+    books: Array<BookDocument>;
+    sales: Array<ChargeDocument>;
     addBook: addBookFunction;
 };
 
 const bookStoreSchema = new mongoose.Schema({
     user: ObjectId,
     name: String,
-    books: [{name: String, price: Number}],
+    books: [{name: String, price: {type: Number, min: 1}}],
+    sales: [Object],
 }, {timestamps: true});
 
 bookStoreSchema.methods.addBook = function (name: string, price: number): Query<BookStoreDocument> {
